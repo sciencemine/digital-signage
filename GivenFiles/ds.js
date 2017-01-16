e1de921d3ab8b1353eec77bb8ba545cc6560b888
trevorlbrooks <trevor@trevorlbrooks.com>
2017-01-16 13:18:31 -0700

    Added files given by Phil Curtiss. Template of the data model is in GivenFiles/playlist.json

---
+++ GivenFiles/ds.js
@@ -0,0 +1,56 @@
+/*
+ * Science Mine Digital Signage Project
+ * JavaScript for DS Project
+ * Phillip J. Curtiss, PhD
+ * Science Mine Board Member
+ * (C) 2016 Science Mine, Inc.
+ * All Rights Reserved
+ */
+
+ // Global Variables
+var vid,			// Main video object
+    pauseButton,	// pauseButton
+	menuButton;		// menuButton
+
+// Init - registers event listeners
+function init() {
+	
+// Obtain elements from the page once it is loaded
+	vid = document.getElementById('bgvid');
+	pauseButton = document.getElementById('vidpause');
+	menuButton = document.getElementById('menuSlide');
+
+// Register listener that cycles through videos upon video 
+// play ending 
+vid.addEventListener('ended', function() {
+    // only functional if "loop" is removed 
+     vid.pause();
+	// to capture IE10
+	vidFade();
+});
+
+pauseButton.addEventListener("click", function() {
+    vid.classList.toggle('stopfade');
+	if (vid.paused) {
+		vid.play();
+		pauseButton.innerHTML = "Pause";
+	} else {
+        vid.pause();
+        pauseButton.innerHTML = "Paused";
+	}
+});
+
+menuButton.addEventListener("click", function() {
+	var me = document.getElementById('menu');
+
+	me.classList.toggle('slide');
+});
+
+}
+
+// Helper functions
+function vidFade() {
+    vid.classList.add("stopfade");
+}
+
+
