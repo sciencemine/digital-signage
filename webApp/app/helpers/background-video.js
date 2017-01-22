ef018635f3210e393ca142f5d1b64c502076f88b
Michael Fryer <michaelfryer@Michaels-MacBook-Pro.local>
2017-01-21 19:42:22 -0700

    Removed unnecessary files

--- webApp/app/helpers/background-video.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Helper.helper(function(url) {
-  var link = Ember.Handlebars.Utils.escapeExpression(url);
-  return Ember.String.htmlSafe(`<video 
-          autoplay playsinline loop><source src="videos/${url}"
-          type="video/mp4"><\\video>`);
-});
