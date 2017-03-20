303c932af641712b6c2e9d80aee41c076938d5e1
Trevor Brooks <trevorlbrooks@users.noreply.github.com>
2017-03-20 13:12:46 -0600

    Update video-list-test.js

--- digital-signage-app-v2/tests/integration/components/video-list-test.js
+++ digital-signage-app-v2/tests/integration/components/video-list-test.js
@@ -21,5 +21,5 @@ test('it renders', function(assert) {
     {{/video-list}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), 'This is a video list\nIs Active: \n      template block text');
 });
