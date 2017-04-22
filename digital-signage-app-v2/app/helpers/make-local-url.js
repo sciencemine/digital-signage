f8f781fe2bd582f4c0bee10b9c4861671436015d
Michael Fryer <michael.d.fryer@gmail.com>
2017-04-22 14:13:08 -0600

    moving to desktop

--- digital-signage-app-v2/app/helpers/make-local-url.js
+++ digital-signage-app-v2/app/helpers/make-local-url.js
@@ -1,7 +1,19 @@
 import Ember from 'ember';
 
-export function makeLocalUrl([arg1, arg2]) {
-  return arg1 + '/' + arg2;
+export function makeLocalUrl([arg1, arg2, arg3, arg4]) {
+  var temporalLocal = '';
+
+  if (parseInt(arg3) === 0) {
+    temporalLocal = '#t=' + arg3;
+  }
+  else if (parseInt(arg4) === 0) {
+    temporalLocal = '#t=,' + arg4;
+  }
+  else if (parseInt(arg3) !== 0 && parseInt(arg4) !== 0) {
+    temporalLocal = '#t=' + arg3 + ',' + arg4;
+  }
+
+  return arg1 + '/' + arg2 + temporalLocal;
 }
 
 export default Ember.Helper.helper(makeLocalUrl);
