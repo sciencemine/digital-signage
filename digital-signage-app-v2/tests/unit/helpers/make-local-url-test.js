6d551b8e6526ce9a5f93cbbab305e6df7e9cb573
Michael Fryer <michael.d.fryer@gmail.com>
2017-04-24 11:02:20 -0600

    made travis happy

--- digital-signage-app-v2/tests/unit/helpers/make-local-url-test.js
+++ digital-signage-app-v2/tests/unit/helpers/make-local-url-test.js
@@ -5,8 +5,26 @@ import { module, test } from 'qunit';
 module('Unit | Helper | make local url');
 
 // Replace this with your real tests.
-test('it works', function(assert) {
-  let result = makeLocalUrl(['it', 'works']);
+test('it works 0 0', function(assert) {
+  let result = makeLocalUrl(['it', 'works', '0', '0']);
   assert.ok(result === 'it/works');
+});
+
+
+test('it works 0 1', function(assert) {
+  let result = makeLocalUrl(['it', 'works', '0', '1']);
+  assert.ok(result === 'it/works#t=,1');
+});
+
+
+test('it works 1 0', function(assert) {
+  let result = makeLocalUrl(['it', 'works', '1', '0']);
+  assert.ok(result === 'it/works#t=1');
+});
+
+
+test('it works 1 2', function(assert) {
+  let result = makeLocalUrl(['it', 'works', '1', '2']);
+  assert.ok(result === 'it/works#t=1,2');
 });
 
--- digital-signage-app-v2/tests/unit/helpers/selected-style-test.js
+++ digital-signage-app-v2/tests/unit/helpers/selected-style-test.js
@@ -6,7 +6,12 @@ module('Unit | Helper | selected style');
 
 // Replace this with your real tests.
 test('it works', function(assert) {
-  let result = selectedStyle([42]);
-  assert.ok(result);
+  let result = selectedStyle(['it', 'works']);
+  assert.ok(result === 'it works');
 });
 
+
+test('baba bobo', function(assert) {
+  let result = selectedStyle(['baba', 'bobo']);
+  assert.ok(result === 'baba bobo');
+});
