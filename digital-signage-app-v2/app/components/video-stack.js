8062a5318b2660e25c48bf9261142ae3c389f631
Zachariah Valenzuela <zvalenzuela@mtech.edu>
2017-05-10 13:19:46 -0600

    fixing merge conflicts

--- digital-signage-app-v2/app/components/video-stack.js
+++ digital-signage-app-v2/app/components/video-stack.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
-});
+export default Ember.Component.extend(
+    {
+        videoCount: Ember.computed('videos', function () {
+            return Object.keys(this.get('videos')).length;
+        }),
+        rootVideo: Ember.computed('videos', function () {
+            return this.get('videos')[Object.keys(this.get('videos'))[0]];
+        }),
+        actions: {
+            stackClicked() {
+                console.log('thing');
+            },
+            incrementSelected() {
+                let arrayLength = this.get('keys').length;
+                let curArrayPos = this.get('selectedVidPos');
+                this.set('selectedVidPos', (curVidPos + 1) % arrayLength);
+            },
+            onHover() {
+
+            }
+        }
+    });
