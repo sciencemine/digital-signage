83be857bd49754340095cb60c7070ae9e385f62d
Michael Fryer <michael.d.fryer@gmail.com>
2017-05-10 15:02:50 -0600

    moving to other pc

--- digital-signage-app-v2/public/models/scanModels.php
+++ digital-signage-app-v2/public/models/scanModels.php
@@ -1,3 +1,42 @@
 <?php
-  
+  ini_set('display_errors', 'On');
+  error_reporting(E_ALL);
+
+  class Server {
+    private $relAddress = '/models/';
+
+    public function serve() {
+      $URI = str_replace($this->relAddress, '', $_SERVER['REQUEST_URI']);
+
+      $method = $_SERVER['REQUEST_METHOD'];
+
+      $explArr = explode('/', $URI);
+
+      $object = $explArr[0];
+      $id = '';
+
+      if (count($explArr) >= 2) {
+        $id = $explArr[1];
+      }
+
+      $item = NULL;
+
+      switch ($method) {
+        case 'GET':
+
+        break;
+        case 'PUT':
+
+        break;
+        case 'POST':
+
+        break;
+        case 'DELETE':
+
+        break;
+      }
+
+      $this->response($item);
+    }
+  }
 ?>
