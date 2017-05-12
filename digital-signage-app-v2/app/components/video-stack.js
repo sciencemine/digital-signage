502c202cfb5c271d62f667fea3b95265d86eb080
Zachariah Valenzuela <zvalenzuela@mtech.edu>
2017-05-12 13:15:38 -0600

    stack css, stack complete

--- digital-signage-app-v2/app/components/video-stack.js
+++ digital-signage-app-v2/app/components/video-stack.js
@@ -5,9 +5,27 @@ export default Ember.Component.extend(
         selectedVidPos: 0,
         keys: [],
         selectedStackIndex: 0,
+        stackStyle: ' ',
+        playerSize: '',
         init() {
             this._super(...arguments);
             this.set('keys', Object.keys(this.get('videos')));
+            switch(this.get('videoCount')){
+                case 1:
+                    break;
+                case 2:
+                    this.set('stackStyle', 'vid-shadows--2');
+                    break;
+                case 3:
+                    this.set('stackStyle', 'vid-shadows--3');
+                    break;
+                case 4:
+                    this.set('stackStyle', 'vid-shadows--4');
+                    break;
+                default:
+                    this.set('stackStyle', 'vid-shadows--4');
+                    break;
+            }
         },
         videoCount: Ember.computed('videos', function () {
             return Object.keys(this.get('videos')).length;
@@ -17,14 +35,13 @@ export default Ember.Component.extend(
         }),
         actions: {
             stackClicked() {
-                console.log('thing');
+                this.get('onClickCallback') (this.get('videos'), this.get('selectedVidPos'));
             },
             getNextVid() {
                 let arrayLength = this.get('keys').length;
                 let curArrayPos = this.get('selectedVidPos');
                 this.set('selectedVidPos', (curArrayPos + 1) % arrayLength);
-                console.log(this.get('selectedVidPos'), curArrayPos, arrayLength); 
-            },
+           },
             hover() {
                 this.get('onHoverCallback') (this.get('selectedStackIndex'));
             }
