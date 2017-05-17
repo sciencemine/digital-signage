3b8cbfc451c7ae120bfe14ad9c5dbd52ac46b118
Michael Fryer <michael.d.fryer@gmail.com>
2017-05-17 13:30:23 -0600

    model-edit route now pulls data from models and logs it

--- model-editor/app/routes/model-edit.js
+++ model-editor/app/routes/model-edit.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  modelfile: "ModelSkeleton",
+
+  beforeModel(params) {
+    let qp = params.queryParams;
+
+    if (qp.modelfile) {
+      this.modelfile = qp.modelfile;
+    }
+  },
+  model() {
+    let path = "models/" + this.modelfile + ".json";
+
+    return Ember.$.getJSON(path);
+  }
 });
