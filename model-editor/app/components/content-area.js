8607154928f1c8f9d45502a50fb2b1b821a0b29d
Michael Fryer <michael.d.fryer@gmail.com>
2017-05-23 19:20:39 -0600

    Pre-processes the attribute and relations content for the properties panel

--- model-editor/app/components/content-area.js
+++ model-editor/app/components/content-area.js
@@ -3,18 +3,43 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   initialModel: null,
   newModel: null,
-  selectedVideo: { },
+  selectedVideo: null,
 
   init() {
-    let component = this;
     this._super(...arguments);
 
     this.set('newModel', this.get('data.modelData'));
 
-    for (var vid in this.get('data.modelData.videos')) {
-      this.set('selectedVideo', this.get('data.modelData.videos')[0]);
+    this.set('selectedVideo', this.get('newModel.videos')[Object.keys(this.get('newModel.videos'))[0]]);
 
-      break;
+    if (this.get('selectedVideo')) {
+      let attributeKeys = this.get('selectedVideo.attributes');
+      let relations = this.get('selectedVideo.relations');
+
+      this.set('selectedVideo.attributes', [ ]);
+      this.set('selectedVideo.relations', [ ]);
+
+      for (var attribute = 0; attribute < attributeKeys.length; attribute++) {
+        let attributeData = this.get('data.modelData.attributes')[attributeKeys[attribute]];
+        let data = { };
+
+        data.name = attributeData.prettyName;
+        data.description = attributeData.description;
+
+        this.get('selectedVideo.attributes').push(data);
+      }
+
+      for (var relation = 0; relation < relations.length; relation++) {
+        let data = { };
+        let relatedVid = this.get('newModel.videos')[relations[relation].relatedId];
+
+        data.name = relatedVid.prettyName;
+        data.description = relatedVid.description;
+        data.difficulty = relations[relation].difficulty;
+        data.attribute = this.get('newModel.attributes')[relations[relation].attributeId].prettyName;
+
+        this.get('selectedVideo.relations').push(data);
+      }
     }
   }
 });
