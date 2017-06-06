59c10e542fa999ab6eb97243aea4fb0b3ab7b9b1
Michael Fryer <michael.d.fryer@gmail.com>
2017-06-05 18:40:55 -0600

    Documented form-contensts and removed unnecessary code

--- model-editor/app/components/form-contents.js
+++ model-editor/app/components/form-contents.js
@@ -1,7 +1,30 @@
+/*******************************************************************************
+ * COMPONENT:
+ *  form-contents
+ *
+ * DESCRIPTION:
+ *  Contents of a form
+ * 
+ * PARAMETERS:
+ *  updateModalCallback - Callback for updating modal data
+ *  prefix - The prefix to use for unique ids
+ *  isInline - If the form is to be inline or now
+ *  showHelp - If the form is to show help messages or not
+ *  showList - If the form is to show lists or not
+ *  noLabel - If the form is to not have labels
+ *  data - The data to populate the form with
+ *  config - The configuration object for the form
+ *
+ * AUTHOR:
+ *  Michael Fryer
+ *
+ * DATE:
+ *  June 5th, 2017
+ ******************************************************************************/
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  inputId: '',
+  tagName: '',
   isInline: false,
   showList: false,
   showHelp: false,
