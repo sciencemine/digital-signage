bba9d8c2782b5cc3e6d20e4f36d0985028caf3e4
Alex Reid <alexreid302@gmail.com>
2017-06-14 12:27:52 -0600

    Changed var to let in for loop of string-append.js to ensure tighter scoping

--- digital-signage-app-v2/app/helpers/string-append.js
+++ digital-signage-app-v2/app/helpers/string-append.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export function stringAppend(params/*, hash*/) {
   let returnString= '';
-  for(var i = 0; i < params.length; i++){
+  for(let i = 0; i < params.length; i++){
     returnString = returnString + params[i] + ' ';
   }
   return returnString;
