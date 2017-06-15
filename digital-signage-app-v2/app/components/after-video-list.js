2a8657983557303398b7211803a7f078c8c3e2e0
Alex Reid <alexreid302@gmail.com>
2017-06-15 11:27:47 -0600

    Added header and extends AbstractList now instead of component

--- digital-signage-app-v2/app/components/after-video-list.js
+++ digital-signage-app-v2/app/components/after-video-list.js
@@ -1,4 +1,52 @@
-import Ember from 'ember';
+/**
+ * COMPONENT: after-video-list
+ *
+ * DESCRIPTION:
+ *  Called after playback of a video has ended, 
+ *  presenting related videos with varying degrees of 
+ *  increasing or decreasing difficulty
+ *
+ * PARAMETERS
+ *  
+ * 
+ *  data - object with keys of attributes and video keys replaced with objects as follows:
+ *  "": {
+ *    "prettyName": "",
+ *    "description": "",
+ *    "x": 0,
+ *    "y": 0,
+ *    "videos": [
+ *      {
+ *        "prettyName": "",
+ *        "description": "",
+ *        "attributes": [ ],
+ *        "relations": [
+ *          {
+ *            "relatedId": "",
+ *            "difficulty": 1,
+ *            "attributeId": ""
+ *          }
+ *        ],
+ *        "full": {
+ *          "fileIdentifier": "",
+ *          "isUrl": false,
+ *          "attribution": ""
+ *        },
+ *        "teaser": {
+ *          "fileIdentifier": "",
+ *          "isUrl": false,
+ *          "attribution": ""
+ *        }
+ *      }
+ *    ]
+ *  }
+ *
+ * @authors Michael Fryer, Zach Valenzuela, Alex Reid
+ * @date 6/15/2017
+ */
 
-export default Ember.Component.extend({
+import AbstractList from './abstract-list';
+export default AbstractList.extend({
+
+
 });
