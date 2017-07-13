f99c3a08d3c69df31cd2cf34929d14fdd8595ee8
Michael Fryer <michael.d.fryer@gmail.com>
2017-07-13 12:47:44 -0600

    Adds a message for unsupported versions being uploaded

--- model-editor/app/controllers/model-select.js
+++ model-editor/app/controllers/model-select.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  notify: Ember.inject.service(),
+  
   modelInformation: { },
   backgroundVideos: [ ],
 
@@ -16,7 +18,16 @@ export default Ember.Controller.extend({
         let fr = new FileReader();
         
         fr.onload = function() {
-          console.log(JSON.parse(fr.result));
+          let modelData = JSON.parse(fr.result);
+          
+          if (this.get('modelInformation').versions.find(function(version) {
+            return modelData.config.version === version;
+          })) {
+            this.get('notify').warning("The version of the selected model is not supported.", {
+              radius: true,
+              closeAfter: 10 * 1000
+            })
+          }
         };
         
         fr.readAsText(file);
