cbaed0c4e60c6e79c56c795e639f741290c918e9
Michael Fryer <michael.d.fryer@gmail.com>
2017-07-14 12:04:36 -0600

    model select plays audio when a box is hovered. added callback for when the stack is selected as opposed to the entire box

--- digital-signage-app-v2/app/components/model-select-element.js
+++ digital-signage-app-v2/app/components/model-select-element.js
@@ -1,6 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: "button",
+  classNames: [ "list-group-item" ],
+  
+  muted: true,
+  
+  mouseEnter() {
+    this.set('muted', false);
+  },
+  mouseLeave() {
+    this.set('muted', true);
+  },
   actions: {
     doNothing() {
       
