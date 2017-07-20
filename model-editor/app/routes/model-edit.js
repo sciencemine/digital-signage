e218f0d3bf9c7a00b4517291fa888d115191a559
Michael Fryer <michael.d.fryer@gmail.com>
2017-07-20 13:11:55 -0600

    Added `ModelDefaultsX.X.json` to allow updating outdated models to newer ones

--- model-editor/app/routes/model-edit.js
+++ model-editor/app/routes/model-edit.js
@@ -5,6 +5,7 @@ const { inject: { service } } = Ember;
 export default Ember.Route.extend({
   modelService: service(),
   visData: service(),
+  notify: service(),
   
   beforeModel() {
     let modelService = this.get('modelService');
@@ -15,5 +16,19 @@ export default Ember.Route.extend({
     else {
       this.get('visData').clear();
     }
+    
+    let path = "models/ModelDefaults" +
+                modelService.get('modelInformation.newestVersion') + ".json";
+    
+    return (function(route) {
+      return Ember.$.getJSON(path).then((data) => {
+        modelService.loadDefaultValues(data);
+      }).fail(() => {
+        route.get('notify').warning("Could not pull data to update model. Using old version.", {
+          radius: true,
+          closeAfter: 10 * 1000
+        })
+      });
+    }) (this);
   }
 });
--- model-editor/app/services/model-service.js
+++ model-editor/app/services/model-service.js
@@ -3,15 +3,63 @@ import Ember from 'ember';
 export default Ember.Service.extend({
   modelInformation: null,
   modelData: null,
+  defaultValues: null,
   modelConfig: null,
-  
+ 
+  updateModel() {
+    this.updateConfig();
+    this.updateVideos();
+    this.updateAttributes();
+  },
+  updateConfig() {
+    this.insertKeys('.config');
+  },
+  updateVideos() {
+    let keys = Object.keys(this.get('modelData.videos'));
+    
+    keys.forEach(function(key) {
+      this.insertKeys(`.videos.${key}`, '.videos.default');
+    }, this);
+  },
+  updateAttributes() {
+    let keys = Object.keys(this.get('modelData.attributes'));
+    
+    keys.forEach(function(key) {
+      this.insertKeys(`.attributes.${key}`, '.attributes.default');
+    }, this);
+  }, 
+  insertKeys(dataPath = "", configPath = "") {
+    configPath = (configPath ? configPath : dataPath);
+
+    let modelData = this.get(`modelData${dataPath}`);
+    let defaultValues = this.get(`defaultValues${configPath}`);
+    let objKeys = Object.keys(modelData);
+
+    this.set(`modelData${dataPath}`, Ember.assign(Ember.copy(defaultValues, true), modelData));
+
+    objKeys.forEach(function(key) {
+      let newData = modelData[key];
+      
+      if (Ember.typeOf(newData) === 'object') {
+        this.insertKeys(`${dataPath}.${key}`, `${configPath}.${key}`);
+      }
+      else if (Ember.isArray(newData) && Ember.isPresent(newData)) {
+        newData.forEach(function(arrData, arrKey) {
+          if (Ember.typeOf(arrData) === 'object') {
+            this.insertKeys(`${dataPath}.${key}.${arrKey}`, `${configPath}.${key}.${0}`);
+          }
+        }, this);
+      }
+    }, this);
+  },
   init() {
     this._super(...arguments);
     
     this.setProperties({
       modelData: null,
       modelInformation: null,
-      modelConfig: null
+      modelConfig: null,
+      defaultValues: null
     });
   },
   loadModelData(data) {
@@ -22,6 +70,11 @@ export default Ember.Service.extend({
   },
   loadModelConfig(data) {
     this.set('modelConfig', data);
+  },
+  loadDefaultValues(data) {
+    this.set('defaultValues', data);
+  
+    this.updateModel();
   },
   update(path, data) {
     let destData = this.get(path);
