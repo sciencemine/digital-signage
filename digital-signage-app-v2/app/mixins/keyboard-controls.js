4960f9a8b1cbeb50bf5ad313c673d3e5a65ca742
Michael Fryer <michael.d.fryer@gmail.com>
2017-07-26 14:48:22 -0600

    keyboard now only pulls the keyboard object once and just stores it

--- digital-signage-app-v2/app/mixins/keyboard-controls.js
+++ digital-signage-app-v2/app/mixins/keyboard-controls.js
@@ -14,56 +14,30 @@ import Ember from 'ember';
 export default Ember.Mixin.create({
   keyboard: null,
   focus: false,
-  /**
-   * Called from keyPress event when the select button is pressed
-   *
-   * @param{event} event
-   */
+
   select: function(event) {
     console.log('select() needs to be implemented in subcomponent', event);
   },
-  /**
-   * Called from keyPress event when the previous button is pressed
-   *
-   * @param{event} event
-   */
   goPrevious: function(event) {
     console.log('goPrevious() needs to be implemented in subcomponent', event);
   },
-  /**
-   * Called from keyPress event when the cancel button is pressed
-   *
-   * @param{event} event
-   */
   cancel: function(event) {
     console.log('cancel() needs to be implemented in subcomponent', event);
   },
-  /**
-   * Called from keyPress event when the next button is pressed
-   *
-   * @param{event} event
-   */
   goNext: function(event) {
     console.log('goNext() needs to be implemented in subcomponent', event);
   },
-  /**
-   * Called from didRender and changeFocusObserver. updates the focus of the 
-   * DOM to this element
-   *
-   */
   updateFocus: function(param) {
     if (param) {
       this.$().attr('tabindex', 10);
       this.$().focus();
-    }//if
+    }
     else {
       this.$().attr('tabindex', -10);
       this.$().blur();
-    }//else
+    }
   },
   
-  //adds an observer for the parameter that was passed. fires when it is changed
-  //up a level. allows for change of focus to the lists
   changeFocusObserver: Ember.observer('focus', function() {
     this.updateFocus(this.get('focus'));
   }),
@@ -71,17 +45,18 @@ export default Ember.Mixin.create({
   //checks input on key down to see if it is valid
   keyDown(event) {
     if (this.$().is(':focus')) {
+      let keyboard = this.get('keyboard');
       switch (String.fromCharCode(event.keyCode).toLowerCase()) {
-        case this.get('keyboard.select').toLowerCase():
+        case keyboard.select.toLowerCase():
           this.select(event);
           break;
-        case this.get('keyboard.previous').toLowerCase():
+        case keyboard.previous.toLowerCase():
           this.goPrevious(event);
           break;
-        case this.get('keyboard.cancel').toLowerCase():
+        case keyboard.cancel.toLowerCase():
           this.cancel(event);
           break;
-        case this.get('keyboard.next').toLowerCase():
+        case keyboard.next.toLowerCase():
           this.goNext(event);
           break;
       }
